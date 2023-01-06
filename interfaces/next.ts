46151bbeec8da5c243a55490529e17b815c3c33b
bivas-biswas <bivasbiswas1999@gmail.com>
2023-01-06 18:02:33 +0530

    :rotating_light: fix lint

--- interfaces/next.ts
+++ interfaces/next.ts
@@ -1,13 +1,9 @@
-import type { LayoutProps } from 'components'
 import type { NextPage } from 'next'
 import type { AppProps as NextAppProps } from 'next/app'
-import type { NextSeoProps } from 'next-seo'
 import { ReactElement } from 'react'
 
 export type AppPage = NextPage & {
   (): ReactElement
-  layout?: LayoutProps
-  seo?: NextSeoProps
 }
 
 export type AppProps = NextAppProps & {
